404c2b0210f10b4bbb0cd3456db5b636da1264e5
juanPabloCesarini <jpablocesarini@gmail.com>
2019-06-22 01:38:23 -0300

    vers 3.22.06.2019

--- ejercicioPuestoJR/src/concesionaria/EjercicioONREADY.java
+++ ejercicioPuestoJR/src/concesionaria/EjercicioONREADY.java
@@ -6,17 +6,16 @@ import java.util.ArrayList;
  *
  * @author Juan Pablo
  */
+/*TODO: mostrarModeloY() - mostrarExtra() - formatear importe */
 public class EjercicioONREADY {
     
     public static void main(String[] args) {
         Sucursal suc1 = new Sucursal();
         suc1.crearSucursal();
         suc1.mostrarVehiculos();
-      /*  mostrarDatos();
-        mostrarMinMax();
-        mostrarModeloY();
-        mostrarExtra();*/
-    
+        suc1.mostrarMinMax();
+               
+           
     } 
 }
 
--- ejercicioPuestoJR/src/concesionaria/Sucursal.java
+++ ejercicioPuestoJR/src/concesionaria/Sucursal.java
@@ -1,6 +1,9 @@
 
 package concesionaria;
 
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.text.NumberFormat;
 import java.util.ArrayList;
 
 /**
@@ -8,6 +11,7 @@ import java.util.ArrayList;
  * @author Juan Pablo
  */
 public class Sucursal implements Consultor{
+  
     ArrayList<Vehiculo> listaVehiculos = new ArrayList();
    
     public void crearSucursal(){
@@ -21,21 +25,65 @@ public class Sucursal implements Consultor{
         auto1.setPuertas("4");
         auto1.setPrecio(200000);
         
+       
+        auto2.setMarca("Peugeot");
+        auto2.setModelo("208");
+        auto2.setPuertas("5");
+        auto2.setPrecio(250000);
+        
+                
+        moto1.setMarca("Honda");
+        moto1.setModelo("Titan");
+        moto1.setCilindradas("125c");
+        moto1.setPrecio(60000);
+        
+     
+        moto2.setMarca("Yamaha");
+        moto2.setModelo("YBR");
+        moto2.setCilindradas("160c");
+        moto2.setPrecio(80500.50);
+        
         listaVehiculos.add(auto1);
-        
-        moto1.setMarca("Yamaha");
-        moto1.setModelo("206");
-        moto1.setCilindradas("160c");
-        moto1.setPrecio(200000);
-        
         listaVehiculos.add(moto1);
+        listaVehiculos.add(auto2);
+        listaVehiculos.add(moto2);
     }
 
     @Override
     public void mostrarVehiculos() {
         int i;
         for (i=0; i<listaVehiculos.size(); i++){
-            System.out.println ("Marca: "+listaVehiculos.get(i).getMarca()  );  
+            System.out.println (listaVehiculos.get(i).toString() + " // Precio: " + listaVehiculos.get(i).getPrecio() );
         }
     }
-}
+    
+    public void mostrarMinMax(){
+        int i;
+        double min = 0;
+        double max = 999999;
+        String marcaMax = "";
+        String modeloMax = "";
+        String marcaMin = "";
+        String modeloMin = "";
+        
+        for (i=0; i<listaVehiculos.size(); i++){
+            if (listaVehiculos.get(i).getPrecio()>min){
+                min = listaVehiculos.get(i).getPrecio();
+                marcaMax = listaVehiculos.get(i).getMarca();
+                modeloMax = listaVehiculos.get(i).getModelo();
+            }
+            if (listaVehiculos.get(i).getPrecio()<max){
+                max = listaVehiculos.get(i).getPrecio();
+                marcaMin = listaVehiculos.get(i).getMarca();
+                modeloMin = listaVehiculos.get(i).getModelo();
+            }
+        }
+        
+        System.out.println("============================");
+        System.out.println("Vehiculo más caro: " + marcaMax + " " + modeloMax);
+        System.out.println("Vehiculo más barato: " + marcaMin + " " + modeloMin);
+    }
+        
+        
+    }
+
